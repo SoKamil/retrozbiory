1e112f6ebb8ae5a01c0876c756849b4d7bd680d7
Kamil Soćko <kamxsoc@gmail.com>
2018-10-21 22:02:09 +0200

    Add: image preload progress bar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div id="app">
     <image-preloader :srcs="getAllImagesArray()" @loaded="loadedOne" @loaded-all="preloadedAllImages"/>
-    <div class="masonry" v-if="imagesPreloaded" v-masonry transition-duration="0" item-selector=".shelfItem">
-      <shelf-item v-masonry-tile v-for="(item, index) in retrozbiory" :key="index" :tooltip="item.title" :thumbnail="item.images[0]" @click.native="openItemModal(index)"></shelf-item>
-    </div>
+    <transition name="fade">
+      <while-loading v-if="!imagesPreloaded" :progress="preloadingProgress"/>
+      <div class="masonry" v-if="imagesPreloaded" v-masonry transition-duration="0" item-selector=".shelfItem">
+        <shelf-item v-masonry-tile v-for="(item, index) in retrozbiory" :key="index" :tooltip="item.title" :thumbnail="item.images[0]" @click.native="openItemModal(index)"></shelf-item>
+      </div>
+    </transition>
     <modal v-if="showModal" @close="showModal = false" :retrozbiory="retrozbiory" :currentModal="currentModal"/>
   </div>
 </template>
@@ -11,6 +14,7 @@
 <script>
 import modal from './components/modal.vue'
 import shelfItem from './components/shelfItem.vue'
+import whileLoading from './components/whileLoading.vue'
 
 import retrozbiory from './assets/retrozbiory.json'
 
@@ -18,14 +22,17 @@ export default {
   name: 'app',
   components: {
     modal,
-    shelfItem
+    shelfItem,
+    whileLoading
   },
   data () {
     return {
+      // additionalImages: [''],
       currentModal: null,
       imagesPreloaded: false,
-      showModal: false,
-      retrozbiory: retrozbiory
+      preloadingProgress: 0,
+      retrozbiory: retrozbiory,
+      showModal: false
     }
   },
   created () {
@@ -36,6 +43,7 @@ export default {
       this.showModal = true
     },
     loadedOne (e) {
+      this.preloadingProgress = e.progress
       console.log('loaded one',e)
     },
     preloadedAllImages () {
@@ -55,4 +63,18 @@ export default {
 </script>
 
 <style lang="stylus">
+#app {
+  position: relative
+}
+
+#whileLoading {
+  position: absolute
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
 </style>
---
+++ src/components/whileLoading.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="whileLoading">
+    <transition name="fade">
+        <div class="progress-container" v-if="progress != 0">
+            <div class="progress" v-bind:style="{width: progress + '%'}"></div>
+        </div>
+    </transition>
+  </div>
+  </div>
+</template>
+<script>
+export default {
+    props: ['progress'],
+}
+</script>
+<style lang="stylus">
+#whileLoading {
+    display:flex
+    justify-content:center
+    align-items:center
+    width: 100%
+    height: 500px
+}
+.progress-container {
+  width: 100%
+  background-color: #eaeaea
+}
+.progress {
+  background-color: #01afe4
+  height: 24px
+  transition: all ease-out 0.4s
+}
+</style>
